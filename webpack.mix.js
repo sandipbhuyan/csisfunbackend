4238e070766a5162b947e9807c2efbaf3e7777fd
sandipbhuyan <sandipbhuyan@gmail.com>
2020-11-14 22:45:27 +0530

    Set up laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,18 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .disableNotifications()
+    .browserSync({
+        proxy: 'localhost:8000',
+        files: [
+            'app/**/*',
+            'public/**/*',
+            'resources/**/*',
+            'routes/**/*'
+        ]
+    })
+
+if (mix.inProduction()) {
+    mix.version();
+}
