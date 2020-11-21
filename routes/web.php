9a9ee983379a32f1131fe6b950a8a27fb92dc63e
sandipbhuyan <sandipbhuyan@gmail.com>
2020-11-21 22:58:52 +0530

    Add category and branch section

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,21 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function(){
+
+    // BranchS
+    Route::resource('branch', 'BranchController');
+    Route::get('branch/publish/{id}', 'BranchController@publish')->name('branch.publish');
+    Route::get('branch/unpublish/{id}', 'BranchController@unpublish')->name('branch.unpublish');
+
+    // PHOTOS
+    Route::resource('category', 'CategoryController');
+    Route::get('category/create/{branch_id}', 'CategoryController@createByBranch')->name('category.createByBranch');
+    Route::get('category/publish/{id}', 'CategoryController@publish')->name('category.publish');
+    Route::get('category/unpublish/{id}', 'CategoryController@unpublish')->name('category.unpublish');
+
+});
+
 Route::middleware(['maintenance'])->prefix(env('MAINTENANCE_URL').'/{password}')->group(function() {
     Route::get('/', 'SetupController@getMaintenance');
     Route::post('/', 'SetupController@postMaintenance')->name('postmn');
