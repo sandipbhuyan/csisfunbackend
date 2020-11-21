4cd0f7c436e3956a101c77ee98c831a4184521ae
sandipbhuyan <sandipbhuyan@gmail.com>
2020-11-21 21:51:31 +0530

    Add maintenance method functionality

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['maintenance'])->prefix(env('MAINTENANCE_URL').'/{password}')->group(function() {
+    Route::get('/', 'SetupController@getMaintenance');
+    Route::post('/', 'SetupController@postMaintenance')->name('postmn');
+});
