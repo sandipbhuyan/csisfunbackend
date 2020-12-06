03dffba0ed3a2ee163f16e837471d368486e7279
sandipbhuyan <sandipbhuyan@gmail.com>
2020-12-06 13:16:18 +0530

    Add parseDown to the project

--- app/Http/Controllers/PublicEndpoint.php
+++ app/Http/Controllers/PublicEndpoint.php
@@ -7,6 +7,8 @@ use App\Post;
 use App\Type;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Parsedown;
+use Storage;
 
 class PublicEndpoint extends Controller
 {
@@ -27,6 +29,8 @@ class PublicEndpoint extends Controller
     }
 
     public function getPost($id) {
-        return Post::find($id);
+        $p = Post::find($id);
+        $p->content = Parsedown::instance()->text(Storage::disk('general_uploads')->get($p->content));
+        return $p;
     }
 }
